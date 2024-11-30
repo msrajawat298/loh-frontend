a18e0e8dc03c8809111cc48775f4f8994be111d7
Mayank Singh Kushwah <msrajawat298@gmail.com>
2024-11-30 23:26:25 +0530

    chore: update GitHub workflows and configuration, change favicon, and remove unused assets

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,13 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern', // or "modern"
+      },
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
